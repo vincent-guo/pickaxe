1fdf32ad1face7156475c83ee383c0d1650321d7
Vincent Guo <vincent.weiguo@gmail.com>
2015-04-05 16:30:05 -0700

    Add intro/hash_with_symbol_keys_19.rb

---
+++ intro/hash_with_symbol_keys_19.rb
@@ -0,0 +1,10 @@
+inst_section = {
+  cello:        'string',
+  clarinet:     'woodwind',
+  drum:         'percussion',
+  oboe:         'woodwind',
+  trumpet:      'brass',
+  violin:       'string'
+}
+
+puts "An oboe is a #{inst_section[:oboe]} instrument"
